8f2c2266dfbd4c91500e995cc5525ac730af801d
Kushang Gajjar <g.kushang@gmail.com>
2016-10-23 03:01:18 -0400

    user info

--- public/containers/user-information.js
+++ public/containers/user-information.js
@@ -11,24 +11,6 @@ class UserInformation extends Component {
         }
 
         return (
-            // <div className="col-sm-8 well margin-well">
-            //     <p className="user-info"><i className="fa fa-paypal"> </i> {this.props.authJson.type} </p>
-            //     <p className="user-info"><i className="fa fa-envelope"> </i> {this.props.authJson.emailAddress}</p>
-            //     <p className="user-info"><i className="fa fa-user-secret"> </i> {this.props.authJson.accountNumber}</p>
-            //     <p className="user-info"><i className="fa fa-user"> </i> {this.props.authJson.firstName}</p>
-            //     <p className="user-info"><i className="fa fa-home"> </i> {this.props.authJson.homeAddress1} </p>
-            //     <p className="user-info"><i className="fa fa-mobile"> </i> {this.props.authJson.mobilePhone} </p>
-            //     <p className="user-info"><i className="fa fa-phone"> </i> {this.props.authJson.homePhoneNumber} </p>
-            //     <p className="user-info"><i className="fa fa-cc-visa"> </i> 7373 3939 3784 6268</p>
-            //     <p className="user-info"><i className="fa fa-cc-mastercard"> </i> 7373 3939 3784 6268</p>
-            //     <p className="user-info"><i className="fa fa-cc-amex"> </i> 7373 3939 3784 6268</p>
-            //     <p className="user-info"><i className="fa fa-cc-discover"> </i> 7373 3939 3784 6268</p>
-            //     <p className="user-info"><i className="fa fa-question-circle"> </i> {this.props.authJson.securityAnswer1} </p>
-            //     <p className="user-info"><i className="fa fa-check-circle"> </i> {this.props.authJson.securityAnswer1} </p>
-            //     <p className="user-info"><i className="fa fa-question-circle"> </i> {this.props.authJson.securityAnswer2} </p>
-            //     <p className="user-info"><i className="fa fa-check-circle"> </i> {this.props.authJson.securityAnswer2} </p>
-            // </div>
-
             <div className="container-fluid user-info-panel">
                 <div className="col-sm-9">
                     <p className="user-info type-info text-center page-header"><i className="fa fa-paypal"> </i> {this.props.authJson.type} </p>
@@ -104,8 +86,3 @@ function mapStateToProps(state) {
 }
 
 export default connect(mapStateToProps)(UserInformation)
-
-
-// <p className="list-group-item"><i className="fa fa-phone"> </i> 000-000-0000 </p>
-//     <p className="list-group-item"><i className="fa fa-envelope"> </i> john@example.com</p>
-// <p className="list-group-item"><i className="fa fa-cc-visa"> </i> 7373 3939 3784 6268</p>
